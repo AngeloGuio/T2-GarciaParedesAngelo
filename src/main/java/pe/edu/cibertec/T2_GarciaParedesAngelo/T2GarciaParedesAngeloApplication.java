d79a5d1d81008fb498cac7370f6cfd5652a15b18
AngeloGarcia <guioangelo.garcia@gmail.com>
2024-06-23 01:07:25 -0500

    commit

---
+++ src/main/java/pe/edu/cibertec/T2_GarciaParedesAngelo/T2GarciaParedesAngeloApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.T2_GarciaParedesAngelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2GarciaParedesAngeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2GarciaParedesAngeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/T2_GarciaParedesAngelo/T2GarciaParedesAngeloApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.T2_GarciaParedesAngelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2GarciaParedesAngeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
