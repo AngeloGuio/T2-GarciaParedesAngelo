1dfa11659e68ce9fc578ebaf4577e7e7327f4e48
AngeloGarcia <guioangelo.garcia@gmail.com>
2024-06-23 11:45:41 -0500

    commit iniciando los controllers

---
+++ src/main/java/pe/edu/cibertec/T2_GarciaParedesAngelo/controller/EspecialidadController.java
@@ -0,0 +1,11 @@
+package pe.edu.cibertec.T2_GarciaParedesAngelo.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping(path = "api/v1")
+public class EspecialidadController {
+}
---
+++ src/main/java/pe/edu/cibertec/T2_GarciaParedesAngelo/controller/MedicoController.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.T2_GarciaParedesAngelo.controller;
+
+public class MedicoController {
+}
